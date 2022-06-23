86370fd877a0221425b053330ad5232a8becc9d1
ErkhanDV <erhan.denis@list.ru>
2022-06-23 09:39:25 +0500

    complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let newStr = '';
+  const str = Math.abs(n).toString();
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return Number(newStr);
 }
